bc13d9848d67b59de23e30cc17bcf4e228f1f973
rbq <rbq@gmx.de>
2009-10-15 18:24:22 +0200

    added iPhone version

--- uph.rb
+++ uph.rb
@@ -1,11 +1,16 @@
 #!/usr/bin/env ruby
 %w'rubygems sinatra haml simple-rss open-uri icalendar date htmlentities'.each { |l| require l }
 
+get '/', :agent => /AppleWebKit.*Mobile/ do
+  redirect '/iphone'
+end
+
 get '/' do
   content_type 'text/html', :charset => 'utf-8'
   response.headers['Cache-Control'] = 'public, max-age=1800'
   haml :index
 end
+
 
 get '/ical' do
   content_type 'text/calendar', :charset => 'utf-8'
@@ -33,16 +38,66 @@ get '/ical' do
   cal.to_ical
 end
 
+
+get '/iphone' do
+  content_type 'text/html', :charset => 'utf-8'
+  response.headers['Cache-Control'] = 'public, max-age=1800'
+  @entries = parse_rss(fetch_rss.entries)
+  haml :index_iphone, :layout => :layout_iphone
+end
+
+
 get '/stylesheets/:name.css' do
   content_type 'text/css', :charset => 'utf-8'
   response.headers['Cache-Control'] = 'public, max-age=1800'
   sass :"stylesheets/#{params[:name]}"
 end
 
+
 not_found do
   redirect '/'
 end
 
+
 error do
   'Fehler: ' + env['sinatra.error'].name
+end
+
+
+helpers do
+  def fetch_rss
+    rss = SimpleRSS.parse open('http://www.unperfekthaus.de/feed/rss.xml')
+  end
+  
+  def parse_rss(entries)
+    he  = HTMLEntities.new
+    entries.map do |entry|
+      if entry.title.match /(\d{1,2}\:\d{1,2})(-(\d{1,2}\:\d{1,2}))? (Raum ([A-Z0-9]+)\: )?(.*)/
+        {
+          :id      => entry.guid,
+          :start   => $1,
+          :end     => $3,
+          :room    => $5,
+          :title   => he.decode($6),
+          :description => nil,
+          :url     => entry.link
+        }
+      else
+        {
+          :title   => entry.title
+        }
+      end
+    end
+  end
+
+  class String
+    def word_shorten(length = 50)
+      if self.length >= length 
+        short = self[0, length-2].split(/\s/)
+        short[0, short.length-1].join(' ') + ' …'
+      else 
+        self
+      end
+    end
+  end
 end
