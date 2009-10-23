2f5d2a626f847ade9c2e908f53bbde276ccd6bc7
rbq <rbq@gmx.de>
2009-10-24 00:14:00 +0200

    daily update

--- lib/uph_event.rb
+++ lib/uph_event.rb
@@ -1,7 +1,7 @@
 class UphEvent
   def initialize(entry)
     he  = HTMLEntities.new
-    if entry.title.match \
+    if entry.title.sub(/-\?{1,3}/, '').match \
       /(\d{1,2}\:\d{1,2})(-(\d{1,2}\:\d{1,2}))? (Raum ([a-zA-Z0-9]+)\: )?(.*)/
       @guid  = entry.guid
       @start = $1
@@ -28,7 +28,7 @@ class UphEvent
   
   def sanitize_title(t)
     t.sub!(/ \(.*/, '')
-    t.sub!(/`s/, '\'s')
+    t.sub!(/[`´]s/, '\'s')
     t.sub!(/Probe - /, 'Probe: ')
     
     case t
@@ -37,15 +37,18 @@ class UphEvent
     when /CMS-Talk/; 'CMS-Talk'
     when /Drupaletics/; 'Drupaletics'
     when /FMA \/ Kali/; 'FMA / Kali'
+    when /Geschlossene Feier/; 'Geschlossene Feier'
     when /Geschlossene Gess?ellschaft/; 'Geschlossene Gesellschaft'
     when /Grosses Dinner-Buffet/; 'Grosses Dinner-Buffet'
     when /e-stylez/; 'e-stylez'
     when /Jeet Kune Do/; 'Jeet Kune Do'
     when /LIEDERMACHER-PROBE/; 'Probe: Liedermacher'
     when /Linuxabend/; 'Linuxabend'
+    when /Lotuscafe/; 'Lotuscafe'
     when /Matthias Scheidig Design/; 'Matthias Scheidig Design'
     when /Physik und Elektroniklabor/; 'Physik-/Elektroniklabor'
     when /Probe: unplugged Rock Pop-Music/; 'Probe: unplugged Rock Pop'
+    when /Sinfonia/; 'Sinfonia'
     when /Möestyle/; 'Airbrush Möestyle'
     when /Ruhrstadtmaler/; 'Der Ruhrstadtmaler'
     else t
