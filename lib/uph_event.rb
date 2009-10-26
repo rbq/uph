4e6b20e77098e44262dc3a0ae52b52f1551f587b
rbq <rbq@gmx.de>
2009-10-27 00:32:48 +0100

    update

--- lib/uph_event.rb
+++ lib/uph_event.rb
@@ -28,6 +28,7 @@ class UphEvent
   
   def sanitize_title(t)
     t.sub!(/ \(.*/, '')
+    t.sub!(/ \/ .*/, '')
     t.sub!(/[`´]s/, '\'s')
     t.sub!(/Probe - /, 'Probe: ')
     t.sub!(/^Offene[rs] /, '')
@@ -41,26 +42,36 @@ class UphEvent
     when /CMS-Talk/; 'CMS-Talk'
     when /Drupaletics/; 'Drupaletics'
     when /Encouraging Training/; 'Encouraging Training'
+    when /e-stylez/; 'e-stylez'
     when /FMA \/ Kali/; 'FMA / Kali'
+    when /Führung durch das Unperfekthaus/; 'Hausführung'
+    when /Fortbildung Spielpädagogik/; 'Fortbildung Spielpädagogik'
     when /Fotostammtisch/; 'Fotostammtisch'
     when /Freie Religion/; 'Freie Religion'
     when /Geschlossene Feier/; 'Geschlossene Feier'
     when /Geschlossene Gess?ellschaft/; 'Geschlossene Gesellschaft'
-    when /Grosses Dinner-Buffet/; 'Grosses Dinner-Buffet'
-    when /e-stylez/; 'e-stylez'
+    when /Grosses Dinner-Buffet/; 'Großes Dinner-Buffet'
     when /Jeet Kune Do/; 'Jeet Kune Do'
+    when /Joomla User Group/; 'Joomla User Group'
     when /Kuvertieraktion/; 'Kuvertieraktion'
+    when /Kultur der Technik/; 'Kultur der Technik'
     when /Latin-Footwork/; 'Latin-Footwork'
     when /LIEDERMACHER-PROBE/; 'Probe: Liedermacher'
     when /Linuxabend/; 'Linuxabend'
     when /Lotuscafe/; 'Lotuscafe'
+    when /Malerei auf dem Weg zur Perfektion/; 'Malerei'
+    when /Massage und integrative Körperarbeit/; 'Massage'
     when /Matthias Scheidig Design/; 'Matthias Scheidig Design'
+    when /Meridian-Yoga/; 'Meridian-Yoga'
     when /My body is my home/; 'My body is my home'
     when /Physik und Elektroniklabor/; 'Physik-/Elektroniklabor'
+    when /Piratenpartei Deutschland/; 'Piratenpartei Deutschland'
     when /Probe: unplugged Rock Pop-Music/; 'Probe: unplugged Rock Pop'
+    when /Projektentwicklung und Beratung/; 'Projektentwicklung'
     when /Salsa, Bachata und Merengue/; 'Salsa, Bachata, Merengue'
     when /Salsakurse/; 'Salsakurse'
     when /Scottys Modellecke/; 'Scottys Modellecke'
+    when /Schreib- und Spielwerkstatt/; 'Schreib- und Spielwerkstatt'
     when /Sinfonia/; 'Sinfonia'
     when /Möestyle/; 'Airbrush Möestyle'
     when /Ruhrstadtmaler/; 'Der Ruhrstadtmaler'
